c78ec3927bc3ebc180b2ebbfc0974db48969323c
Zhong Yi Yan <lucayan0506@gmail.com>
2024-11-15 20:28:10 +0000

    fix smoothingKernelDerivative

--- app/Main2.hs
+++ app/Main2.hs
@@ -29,7 +29,6 @@ data Density = Density{
 
 data GameState = GameState
   { particles :: [Particle],
-    smoothRadius :: Float,
     densities :: [Density],
     randomPoints :: [Point]
   }
@@ -39,21 +38,23 @@ width = 600
 height = 400
 offset = 300
 
-squareSize, defaultRadius ,gravity, collisionDamping, targetDensity, pressureMultiplier :: Float
+smoothRadius, squareSize, defaultRadius ,gravity, collisionDamping, targetDensity, pressureMultiplier :: Float
 defaultRadius = 10
+smoothRadius = 25
 squareSize = 5
--- gravity = 0 
-gravity = -981
+gravity = 0 
+-- gravity = -981
 collisionDamping = 0.2
-targetDensity = 1.2
-pressureMultiplier = 40000
+targetDensity = 3.926795e-6
+pressureMultiplier = 10000000
+
 
 
 
 randomParticle :: ID -> IO Particle
 randomParticle id = do
   x <- randomRIO (-(fromIntegral width / 2 - defaultRadius), fromIntegral width / 2 - defaultRadius)
-  y <- randomRIO (- (fromIntegral height / 2), fromIntegral height / 2)
+  y <- randomRIO (- (fromIntegral height / 2 - defaultRadius), fromIntegral height / 2 - defaultRadius)
   -- m <- randomRIO (1,50)
   -- vx <- randomRIO (-50, 50)  -- Random velocity
   -- vy <- randomRIO (-50, 50)  -- Random velocity
@@ -64,11 +65,11 @@ randomParticles n = mapM randomParticle [1..n]
 
 evenlySpacedParticles :: Float -> [Particle]
 evenlySpacedParticles n = 
-  let positions_index = [ [-300 + (i * 25), -200 + (j * 25), i * n + j]| i <- [1..n],j <- [1..n]]
+  let positions_index = [ [-300 + (i * 24), -200 + (j * 24), i * n + j]| i <- [1..n],j <- [1..n]]
   in map (\[x, y, id] -> Particle (x, y) (0, 0) 1 defaultRadius 0 0 id black) positions_index
 
 updatePosition :: Float -> Particle -> Particle
-updatePosition dt p = p { pos = (x, y + vy * dt) }
+updatePosition dt p = p { pos = (x + vx * dt, y + vy * dt) }
   where
     (x, y) = pos p
     (vx, vy) = vel p
@@ -82,31 +83,32 @@ applyGravity seconds p = p {vel = (vx',vy')}
           -- y' = y + vy' * seconds
 
 applyBoardCollision :: Particle -> Particle
-applyBoardCollision p = p {vel = (vx',vy')}
+applyBoardCollision p = p {pos = (x,y), vel = (vx',vy')}
     where (vx,vy) = vel p
           (x,y) = pos p
           vy'| abs y > abs (fromIntegral height / 2) - defaultRadius  = vy * (-1) * collisionDamping
              | otherwise = vy
           vx'| abs x > abs (fromIntegral width / 2) - defaultRadius = vx * (-1) * collisionDamping
              | otherwise = vx
-          -- x' | abs x > abs (fromIntegral width / 2) - defaultRadius = sign x * ((fromIntegral width / 2) - defaultRadius)
-          --    | otherwise = x
-          -- y' | abs y > abs ((fromIntegral height / 2) - defaultRadius) = sign y * ((fromIntegral height / 2) - defaultRadius)
-          --    | otherwise = y
+          x' | abs x > abs (fromIntegral width / 2) - defaultRadius = sign x * ((fromIntegral width / 2) - defaultRadius - 10)
+             | otherwise = x
+          y' | abs y > abs ((fromIntegral height / 2) - defaultRadius) = sign y * ((fromIntegral height / 2) - defaultRadius - 10)
+             | otherwise = y
 
--- sign :: Float -> Float
--- sign x | x >= 0 = 1
---        | otherwise = -1
+sign :: Float -> Float
+sign x | x >= 0 = 1
+       | otherwise = -1
 
 applyDensity :: Particle -> Particle
 applyDensity p = p {opacity = opacity'}
-  where opacity' = density p 
+  where opacity' = density p * 100
 
 
-calculateDensity :: Point -> GameState -> Float
-calculateDensity (x1,y1) state = sum [ _density p | p <- particles state ]
-    where _density p = mass p * influence p
-          influence p = smoothingKernel (distance $ pos p) (smoothRadius state)
+calculateDensity :: Particle -> GameState -> Float
+calculateDensity p state = sum [ _density otherP | otherP <- particles state, id otherP /= id p && distance (pos otherP) < smoothRadius]
+    where (x1,y1) = pos p
+          _density localP = mass localP * influence localP
+          influence localP = smoothingKernel (distance $ pos localP) smoothRadius
           distance (x,y) = sqrt ((x - x1) * (x - x1) + (y - y1) * (y - y1))
 
 smoothingKernel :: Float -> Float -> Float
@@ -117,23 +119,22 @@ smoothingKernel distance _smoothRadius = (value**3) / volume
 smoothingKernelDerivative :: Float -> Float -> Float
 smoothingKernelDerivative distance _smoothRadius 
   | distance >= _smoothRadius = 0
-  | otherwise = constant * distance * f *f
-    where constant = (-24) / pi * (_smoothRadius**8)
+  | otherwise = constant * distance * f * f
+    where constant = (-24) / (pi * (_smoothRadius**8))
           f = _smoothRadius * _smoothRadius - distance * distance
 
 updateDensity :: GameState -> Particle -> Particle
 updateDensity state p = p {density = density'}
-  where (x,y) = pos p
-        density' = calculateDensity (x,y) state
+  where density' = calculateDensity p state
 
 applyPressure :: Float -> GameState -> Particle -> Particle
 applyPressure dt state p = p {vel = (vx',vy')}
   where (x,y) = pos p
         -- (vx',vy') = customSum [(x,y) ,customMult pressureAcceleration dt]
-        (vx',vy') = customMult pressureAcceleration dt
-        pressureAcceleration 
-          | density p < 0.1 = (1/dt,1/dt)
-          | otherwise = customMult (calculatePressureForce p state) (1 / density p)
+        (vx',vy') 
+          | density p == 0 = vel p
+          | otherwise = customMult pressureAcceleration dt
+        pressureAcceleration = customMult (calculatePressureForce p state) (1 / density p)
         -- limit:: Point -> Point
         -- limit (x1,y1)
         --   | x1 > 100 && y1 > 100 = (100,100)
@@ -149,19 +150,21 @@ convertDensityToPressure _density = pressure
 calculatePressureForce :: Particle -> GameState -> Point
 calculatePressureForce p state = pressureForce
   where (x1,y1) = pos p
-        pressureForce = customSum [ dir (pos otherP) `customMult` (convertDensityToPressure(density otherP) * slope otherP * mass otherP / getDensity otherP) | otherP <- particles state, id otherP /= id p]
+        pressureForce = customSum [ dir (pos otherP) `customMult` (pressure otherP * slope otherP * mass otherP / getDensity otherP) | otherP <- particles state,
+                                                                   id otherP /= id p && distance (pos otherP) < smoothRadius]
         dir (x,y) 
           -- | distance (x,y) < 1 = (1,1)
           | distance (x,y) < 1 = head $ randomPoints state
-          | otherwise = (x - x1, y - y1) 
-          -- | otherwise = customMult (x - x1, y - y1) ( 1/ distance (x,y))
+          -- | otherwise = (x - x1, y - y1) 
+          | otherwise = customMult (x - x1, y - y1) ( 1/ distance (x,y))
         slope localP
           | distance (pos localP) < 1 = 1
-          | otherwise = smoothingKernelDerivative (distance $ pos localP) (smoothRadius state) 
+          | otherwise = smoothingKernelDerivative (distance $ pos localP) smoothRadius
         distance (x,y) = sqrt ((x - x1) * (x - x1) + (y - y1) * (y - y1))
         getDensity localP
           | density localP < 0.1 = 1
           | otherwise = density localP
+        pressure localP =  convertDensityToPressure(density localP)
 
 
 
@@ -169,7 +172,7 @@ calculatePressureForce p state = pressureForce
 drawParticle :: Particle -> Picture
 drawParticle p = pictures [
     translate x y $ color (particleColor p) $ circleSolid (radius p),
-    translate x y $ color (makeColor 0.0078 0.5882 1 (opacity p)) $ circleSolid (radius p + 10)]
+    translate x y $ color (makeColor 0.0078 0.5882 1 0.5) $ circleSolid smoothRadius]
   where (x,y) = pos p
 
 drawDensity :: Density -> Picture
@@ -190,7 +193,7 @@ initialState = do
   -- let d = [ Density (-300 + (i * 25), -200 + (j * 25)) 0| i <- [1..15],j <- [1..15]]
   let d = [ Density (pos x) 0| x <- p]
   
-  return GameState { particles = p, smoothRadius = 3, densities = d, randomPoints = [(1,1),(1,-1),(-1,1),(-1,-1)]}
+  return GameState { particles = p, densities = d, randomPoints = [(1,1),(1,-1),(-1,1),(-1,-1)]}
 
 render :: GameState -> Picture
 render state = Pictures $ map drawDensity (densities state) ++ map drawParticle (particles state)
@@ -198,7 +201,7 @@ render state = Pictures $ map drawDensity (densities state) ++ map drawParticle 
 
 update :: Float -> GameState -> GameState
 update dt state = state { particles = finalParticles, randomPoints = newRandomPoints}
-  -- where particlesWithForces = map (applyPressure dt state . updateDensity state) (particles state)
+  -- where particlesWithForces = map (applyDensity . updateDensity state) (particles state)
   where particlesWithForces = map (applyGravity dt . applyPressure dt state . updateDensity state) (particles state)
         -- finalParticles = particlesWithForces
         finalParticles = map (updatePosition dt . applyBoardCollision) particlesWithForces
@@ -222,21 +225,21 @@ main = do
 --Events
 
 handleEvent ::  Event -> GameState -> GameState
-handleEvent (EventKey (Char 'd') Down _ _) state  = 
-  let newDensities = [Density (x,y) (calculateDensity (x,y) state - targetDensity)| Density (x,y) k <- densities state]
-  in state { densities = newDensities }
+-- handleEvent (EventKey (Char 'd') Down _ _) state  = 
+  -- let newDensities = [Density (x,y) (calculateDensity (x,y) state - targetDensity)| Density (x,y) k <- densities state]
+  -- in state { densities = newDensities }
 
-handleEvent (EventKey (Char 'p') Down _ _) state  = 
-  let newDensities = [Density (x,y) (convertDensityToPressure $ calculateDensity (x,y) state)| Density (x,y) k <- densities state]
-  in state { densities = newDensities }
+-- handleEvent (EventKey (Char 'p') Down _ _) state  = 
+--   let newDensities = [Density (x,y) (convertDensityToPressure $ calculateDensity (x,y) state)| Density (x,y) k <- densities state]
+--   in state { densities = newDensities }
 
 handleEvent (EventKey (Char 'a') Down _ _) state  = 
   let ps = map (applyPressure (1/60) state) (particles state)
   in state { particles = ps }
 
 handleEvent (EventKey (Char 'f') Down _ mousePos) state = unsafePerformIO $ do
-    print [calculatePressureForce p state| p <- init $ particles state]
-    print [density p| p <- init $ particles state]
+    -- print [customMult (calculatePressureForce p state) (1 / density p)| p <- particles state]
+    print [density p| p <- particles state]
     -- print [pos p| p <- particles state]
     return state { particles = particles state}
 
@@ -244,8 +247,11 @@ handleEvent (EventKey (Char 'n') Down _ mousePos) state = state { particles = Pa
 
 handleEvent (EventKey (MouseButton LeftButton) Down _ mousePos) state = unsafePerformIO $ do 
     let newPs = changeColor mousePos red (particles state)
-    
+    -- let distance (x,y) (x1,y1)= sqrt ((x - x1) * (x - x1) + (y - y1) * (y - y1))
+    -- print $ (pos $ closestParticle mousePos (particles state)) 
+    -- print $ smoothingKernel (distance (pos $ closestParticle mousePos (particles state)) (pos $ head $ particles state)) smoothRadius
     print $ density (closestParticle mousePos (particles state)) 
+    -- print $ convertDensityToPressure $ density (closestParticle mousePos (particles state)) 
     print $ calculatePressureForce (closestParticle mousePos (particles state)) state
     print $ vel (closestParticle mousePos (particles state)) 
     print ""
@@ -260,7 +266,7 @@ handleEvent  _ state = state
 --End events
 
 changeColor :: Point -> Color -> [Particle] -> [Particle]
-changeColor _ _ [p] = [p]
+changeColor _ c [p] = [p {particleColor = c}]
 changeColor (x1,y1) c (p:ps) 
   | x1 >= x - defaultRadius && x1 <= x + defaultRadius && y1 >= y - defaultRadius && y1 <= y + defaultRadius = (p {particleColor = c}):ps
   | otherwise = p:changeColor (x1,y1) c ps
@@ -278,7 +284,7 @@ customMult :: Point -> Float -> Point
 customMult (x,y) a = (a * x, a * y)
 
 customSum :: [Point] -> Point
-customSum ((x1,y1):(x2,y2):ps) = customSum ((x1 + x2, y1 + y2):ps)
-customSum [p] = p
 customSum [] = (0,0)
+customSum [p] = p
+customSum ((x1,y1):(x2,y2):ps) = customSum ((x1 + x2, y1 + y2):ps)
 
