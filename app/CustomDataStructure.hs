5915aa85bb7e627672b64aa5608fcac116251dfc
Zhong Yi Yan <lucayan0506@gmail.com>
2024-11-17 17:30:26 +0000

    save particles in a 2d vector to optimise it

---
+++ app/CustomDataStructure.hs
@@ -0,0 +1,62 @@
+module CustomDataStructure where
+
+import qualified Data.Vector as V
+import Graphics.Gloss ( Point, Color )
+
+type Vector2D a = V.Vector [a]
+
+
+type Velocity = (Float, Float)
+type ID = Float
+
+data Particle = Particle
+  { pos :: Point,
+    vel :: Velocity,
+    mass :: Float,
+    radius :: Float,
+    opacity :: Float,
+    density :: Float,
+    id :: ID,
+    particleColor::Color
+  }
+
+instance Show Particle where
+    show (Particle (x,y) _ _ _ _ _ _ _) = show (x,y)
+
+data Density = Density{
+  densityPos:: Point,
+  value :: Float
+}
+
+--functions for 2D Vector
+vector2DToList :: Vector2D a -> [a]
+vector2DToList vec2D = helper (V.toList vec2D)
+    where helper :: [[a]] -> [a]
+          helper [] = []
+          helper [x] = x
+          helper (x:xs) = x ++ helper xs
+
+list2DToVector2D :: [[a]] -> Vector2D a
+list2DToVector2D = V.fromList
+
+map2D :: (a -> b) -> Vector2D a -> Vector2D b
+map2D f = V.map (map f)
+
+length2D :: Vector2D a -> Int
+length2D = V.foldl' addRowLength 0
+    where addRowLength currSum row = currSum + length row
+
+addElementToRow :: Int -> a -> Vector2D a -> Vector2D a
+addElementToRow i newElem grid
+    | i > V.length grid || i < 0 = V.update grid (V.singleton (0, newRow 0))
+    | otherwise = V.update grid (V.singleton (i, newRow i))
+    where
+        newRow localI = newElem : grid V.! localI
+
+getNList :: Int -> Vector2D a -> [a]
+getNList i vec2D 
+    | i < 0 || i >= V.length vec2D = []
+    | otherwise = vec2D V.! i
+
+emptyVector2DWithNRow :: Int ->  Vector2D a
+emptyVector2DWithNRow n = V.replicate n []
--- app/Main.hs
+++ app/Main.hs
@@ -5,51 +5,35 @@ import Graphics.Gloss.Interface.Pure.Game
 import System.Random (randomRIO)
 import Data.IORef
 import Graphics.Gloss.Interface.IO.Game (playIO)
-
 import System.IO.Unsafe (unsafePerformIO)
+import qualified Data.Vector as V
+import CustomDataStructure
 
-type Velocity = (Float, Float)
-type ID = Float
-
-data Particle = Particle
-  { pos :: Point,
-    vel :: Velocity,
-    mass :: Float,
-    radius :: Float,
-    opacity :: Float,
-    density :: Float,
-    id :: ID,
-    particleColor::Color
-  }
-
-data Density = Density{
-  densityPos:: Point,
-  value :: Float
-}
 
 data GameState = GameState
-  { particles :: [Particle],
+  { particles :: Vector2D Particle,
     densities :: [Density],
     randomPoints :: [Point],
     pause :: Bool,
     generateParticles :: Bool,
-    mousePos :: Point
+    mousePos :: Point,
+    showGrid :: Bool,
+    gravity :: Float
   }
 
-width, height, offset :: Int
-width = 300
-height = 200
+width, height, offset, gridColumn :: Int
+width = 600
+height = 400
 offset = 300
+gridColumn = floor (fromIntegral width / squareSize)
 
-smoothRadius, squareSize, defaultRadius ,gravity, collisionDamping, targetDensity, pressureMultiplier :: Float
+smoothRadius, squareSize, defaultRadius , collisionDamping, targetDensity, pressureMultiplier :: Float
 defaultRadius = 5
 smoothRadius = 25
-squareSize = 5
--- gravity = 0 
-gravity = -981 
+squareSize = smoothRadius
 collisionDamping = 0.5
 targetDensity = 2.651827e-3
-pressureMultiplier = 10000000
+pressureMultiplier = 50000000
 
 
 
@@ -61,13 +45,13 @@ randomParticle id = do
   -- m <- randomRIO (1,50)
   -- vx <- randomRIO (-50, 50)  -- Random velocity
   -- vy <- randomRIO (-50, 50)  -- Random velocity
-  return $ Particle (x, y) (0, 0) 1 defaultRadius 0 0 id white
+  return $ Particle (x, y) (0, 0) 1 defaultRadius 0 0 id (makeColor 0.0078 0.5882 1 1)
 
 randomParticles :: Float -> IO [Particle]
 randomParticles n = mapM randomParticle [1..n]
 
 evenlySpacedParticles :: Float -> [Particle]
-evenlySpacedParticles n = 
+evenlySpacedParticles n =
   let positions_index = [[ i , j , i * n + j]| i <- [0..n],j <- [0..n], (dirx,diry) <- []]
   in map (\[x, y, id] -> Particle (x, y) (0, 0) 1 defaultRadius 0 0 id (makeColor 0.0078 0.5882 1 1)) positions_index
 
@@ -77,11 +61,11 @@ updatePosition dt p = p { pos = (x + vx * dt, y + vy * dt) }
     (x, y) = pos p
     (vx, vy) = vel p
 
-applyGravity :: Float -> Particle -> Particle
-applyGravity seconds p = p {vel = (vx',vy')}
+applyGravity :: Float -> GameState -> Particle -> Particle
+applyGravity seconds state p = p {vel = (vx',vy')}
     where (vx,vy) = vel p
           vx' = vx
-          vy' = vy + gravity * seconds
+          vy' = vy + (gravity state) * seconds
           -- x' = x + vx * seconds
           -- y' = y + vy' * seconds
 
@@ -103,14 +87,23 @@ applyBoardCollision p = p {pos = (x',y'), vel = (vx',vy')}
 
 
 calculateDensity :: Particle -> GameState -> Float
-calculateDensity p state = sum [ _density otherP | otherP <- particles state, distance (pos otherP) < smoothRadius]
-    where (x1,y1) = pos p
+calculateDensity p state = sum [ _density otherP | otherP <- ps, distance (pos otherP) < smoothRadius]
+    where ps = getNList (convertPositionToIndex (pos p) + gridColumn) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - gridColumn) (particles state) ++
+               getNList (convertPositionToIndex (pos p) + 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) + gridColumn + 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) + gridColumn - 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - gridColumn + 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - gridColumn - 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p)) (particles state) 
+          (x1,y1) = pos p
           _density localP = mass localP * influence localP
           influence localP = smoothingKernel (distance $ pos localP)
           distance (x,y) = sqrt ((x - x1) * (x - x1) + (y - y1) * (y - y1))
 
 smoothingKernel :: Float -> Float
-smoothingKernel distance 
+smoothingKernel distance
   | distance >= smoothRadius = 0
   | otherwise = (smoothRadius - distance) * (smoothRadius - distance) / volume
     where volume = (pi * (smoothRadius**4)) / 6
@@ -119,7 +112,7 @@ smoothingKernel distance
 --           volume = pi * (_smoothRadius**8) / 4
 
 smoothingKernelDerivative :: Float -> Float
-smoothingKernelDerivative distance 
+smoothingKernelDerivative distance
   | distance >= smoothRadius = 0
   | otherwise = (distance - smoothRadius) * constant
     where constant = 12 / ((smoothRadius**4) * pi)
@@ -151,23 +144,47 @@ convertDensityToPressure _density = pressure
 calculatePressureForce :: Particle -> GameState -> Point
 calculatePressureForce p state = pressureForce
   where (x1,y1) = pos p
-        pressureForce = customSum [ dir (pos otherP) (id otherP) `customMult` (sharedPressure otherP * slope otherP * mass otherP / getDensity otherP) | otherP <- particles state,
-                                                                   id otherP /= id p && distance (pos otherP) < smoothRadius]
+        pressureForce = customSum [ dir (pos otherP) (id otherP) `customMult` (sharedPressure otherP * slope otherP * mass otherP / getDensity otherP) 
+                                                                  | otherP <- ps
+                                                                  , id otherP /= id p && distance (pos otherP) < smoothRadius]
+        ps = getNList (convertPositionToIndex (pos p) + gridColumn) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - gridColumn) (particles state) ++
+               getNList (convertPositionToIndex (pos p) + 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) + gridColumn + 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) + gridColumn - 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - gridColumn + 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - gridColumn - 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p)) (particles state) 
+        
         dir :: Point -> Float -> Point
         dir (x,y) _id
           -- | distance (x,y) < 1 = (1,1)
           | distance (x,y) == 0 = randomPoints state !! (fromEnum _id `mod` 4)
           -- | otherwise = (x - x1, y - y1) 
           | otherwise = customMult (x - x1, y - y1) ( 1/ distance (x,y))
-        slope localP 
+        slope localP
           | distance (pos localP) == 0 = smoothingKernelDerivative 1
           | otherwise = smoothingKernelDerivative (distance $ pos localP)
         distance (x,y) = sqrt ((x - x1) * (x - x1) + (y - y1) * (y - y1))
         getDensity localP
           | density localP < 0.1 = 1
           | otherwise = density localP
-        sharedPressure localP = (convertDensityToPressure(density localP) + convertDensityToPressure (density p)) / 2
+        sharedPressure localP = (convertDensityToPressure (density localP) + convertDensityToPressure (density p)) / 2
 
+
+putParticleToGrid :: [Particle] -> Vector2D Particle -> Vector2D Particle
+putParticleToGrid [] grid = grid
+putParticleToGrid (p:ps) grid = putParticleToGrid ps newGrid
+  where newGrid = addElementToRow (convertPositionToIndex $ pos p) p grid
+
+convertPositionToIndex :: Point -> Int
+convertPositionToIndex (x,y) = floor ((x + (fromIntegral width / 2)) / squareSize) + (floor ((y + fromIntegral height / 2) / squareSize ) * gridColumn)
+
+updateGrid ::  Vector2D Particle -> Vector2D Particle
+updateGrid grid = putParticleToGrid (vector2DToList grid) newGrid
+-- updateGrid grid = map2D  [(p, convertPositionToIndex $ pos p) | p <- vector2DToList grid]
+    where newGrid = emptyVector2DWithNRow (round ((fromIntegral width / squareSize) * (fromIntegral height/ squareSize)))
 
 
 --Main Loop Components
@@ -177,38 +194,45 @@ drawParticle p = pictures [
     -- translate x y $ color (makeColor 0.0078 0.5882 1 0.5) $ circleSolid smoothRadius]
   where (x,y) = pos p
 
-drawDensity :: Density -> Picture
-drawDensity (Density (x,y) k) = translate x y $ color (makeColor r g b a) $ rectangleSolid squareSize squareSize
-  where (r,g,b,a) = customColor 
-        customColor 
-         | k < -0.0001 = (0.1266, 0.5330, 0.6886, 1)
-         | k > 0.001 = (0.8301, 0.2914, 0.2075, 1)
-         | otherwise = (1, 1, 1, 1)
+drawGrid :: Point -> Picture
+drawGrid (x,y) = translate x y $ color white $ rectangleWire squareSize squareSize
 
 initialState :: IO GameState
 initialState = do
-  p <- randomParticles 100
+  p <- randomParticles 1
   -- let p = evenlySpacedParticles 4
   let pixx = fromIntegral width
   let pixy = fromIntegral height
   -- let d = [Density (x,y) 0 | x <- [-pixx,((-pixx) + squareSize)..pixx], y <- [-pixy,((-pixy) + squareSize)..pixy]]
   -- let d = [ Density (-300 + (i * 25), -200 + (j * 25)) 0| i <- [1..15],j <- [1..15]]
   let d = [ Density (pos x) 0| x <- p]
-  
-  return GameState { particles = p, densities = d, randomPoints = [(1,0.1),(1,-1),(-1,1),(-1,-1)], pause = False, generateParticles = False, mousePos = (0,0)}
+
+  return GameState { particles = list2DToVector2D [p], densities = d, randomPoints = [(1,0.1),(1,-1),(-1,1),(-1,-1)], pause = True, generateParticles = False, mousePos = (0,0), showGrid = False, gravity = 0}
 
 render :: GameState -> Picture
--- render state = Pictures $ map drawDensity (densities state) ++ map drawParticle (particles state)
-render state = Pictures $ map drawParticle (particles state)
+-- render state = Pictures $ [drawGrid (x,y) (round (y * 8 + x)) | x <- [0], y <- [0]] 
+render state
+    | showGrid state = Pictures $ [drawGrid (x,y) | x <- [-n + cellSize,-n + 3 * cellSize..n - cellSize], y <- [-m + cellSize,-m + 3 * cellSize..m - cellSize]]
+               ++ vector2DToList (map2D drawParticle (particles state))
+    | otherwise = Pictures $ vector2DToList (map2D drawParticle (particles state))
+  where n = fromIntegral width / 2
+        m = fromIntegral height / 2
+        cellSize = squareSize / 2
 
 update :: Float -> GameState -> GameState
 update dt state = state { particles = finalParticles, randomPoints = newRandomPoints}
-  where newParticles 
-          | generateParticles state = Particle (mousePos state) (0, 0) 1 defaultRadius 0 0 ((fromIntegral $ length $ particles state) + 1.0) (makeColor 0.0078 0.5882 1 1) :particles state
+  where newParticles
+          | generateParticles state = addElementToRow 0 newParticle (particles state)
           | otherwise = particles state
-        particlesWithForces = map (applyGravity dt . applyPressure dt state . updateDensity state) (newParticles)
-        -- finalParticles = particlesWithForces
-        finalParticles = map (updatePosition dt . applyBoardCollision) particlesWithForces
+        newParticle = Particle (mousePos state) (0, 0) 1 defaultRadius 0 0 ((fromIntegral $ length2D $ particles state) + 1.0) (makeColor 0.0078 0.5882 1 1)
+
+        particlesWithGridUpdated = updateGrid newParticles
+        particlesWithForces = map2D (applyGravity dt state . applyPressure dt state . updateDensity state) particlesWithGridUpdated
+
+        finalParticles
+          | pause state = particlesWithForces
+          | otherwise = map2D (updatePosition dt . applyBoardCollision) particlesWithForces
+
         newRandomPoints = tail $ randomPoints state ++ [head $ randomPoints state]
 
 --End main loop components
@@ -229,27 +253,28 @@ main = do
 --Events
 
 handleEvent ::  Event -> GameState -> GameState
--- handleEvent (EventKey (Char 'd') Down _ _) state  = 
-  -- let newDensities = [Density (x,y) (calculateDensity (x,y) state - targetDensity)| Density (x,y) k <- densities state]
-  -- in state { densities = newDensities }
-
--- handleEvent (EventKey (Char 'p') Down _ _) state  = 
---   let newDensities = [Density (x,y) (convertDensityToPressure $ calculateDensity (x,y) state)| Density (x,y) k <- densities state]
---   in state { densities = newDensities }
-
-handleEvent (EventKey (Char 'a') Down _ _) state  = 
-  let ps = map (applyPressure (1/60) state) (particles state)
-  in state { particles = ps }
-
-handleEvent (EventKey (Char 'f') Down _ mousePos) state = unsafePerformIO $ do
+--Debugging tools
+handleEvent (EventKey (Char 'f') Down _ _) state = unsafePerformIO $ do
     -- print [convertDensityToPressure $ density p| p <- particles state]
-    print [customMult (calculatePressureForce p state) (1 / density p)| p <- particles state]
+    let p = closestParticle (mousePos state) (vector2DToList $ particles state)
+    let ps = getNList (convertPositionToIndex (pos p) + gridColumn) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - gridColumn) (particles state) ++
+               getNList (convertPositionToIndex (pos p) + 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) + gridColumn + 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) + gridColumn - 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - gridColumn + 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p) - gridColumn - 1) (particles state) ++
+               getNList (convertPositionToIndex (pos p)) (particles state) 
+    print ps
     return state { particles = particles state}
 
-handleEvent (EventKey (Char 'n') Down _ _mousePos) state = state { particles = Particle _mousePos (0, 0) 1 defaultRadius 0 0 ((fromIntegral $ length $ particles state) + 1.0) (makeColor 0.0078 0.5882 1 1) :particles state}
+handleEvent (EventKey (Char 'n') Down _ _mousePos) state = state { particles = addElementToRow 0 newParticle (particles state) }
+  where newParticle = Particle (mousePos state) (0, 0) 1 defaultRadius 0 0 ((fromIntegral $ length2D $ particles state) + 1.0) (makeColor 0.0078 0.5882 1 1)
 
-handleEvent (EventKey (MouseButton LeftButton) Down _ _mousePos) state = unsafePerformIO $ do 
-    let newPs = changeColor _mousePos red (particles state)
+
+handleEvent (EventKey (MouseButton LeftButton) Down _ _mousePos) state = unsafePerformIO $ do
+    let newPs = map2D (changeColor _mousePos red) (particles state)
     -- let distance (x,y) (x1,y1)= sqrt ((x - x1) * (x - x1) + (y - y1) * (y - y1))
     -- print $ (pos $ closestParticle mousePos (particles state)) 
     -- print $ smoothingKernel (distance (pos $ closestParticle mousePos (particles state)) (pos $ head $ particles state)) smoothRadius
@@ -258,31 +283,43 @@ handleEvent (EventKey (MouseButton LeftButton) Down _ _mousePos) state = unsafeP
     -- print $ calculatePressureForce (closestParticle mousePos (particles state)) state
     -- print $ vel (closestParticle mousePos (particles state)) 
     -- let p = closestParticle mousePos (particles state)
-    print $ smoothingKernelDerivative 0
+    let convertPositionToIndex (x,y) = floor ((x + (fromIntegral width / 2)) / squareSize) + (floor ((y + fromIntegral height / 2) / squareSize ) * floor (fromIntegral width / squareSize))
+
+    print $ convertPositionToIndex _mousePos
+
     -- print $ applyPressure2 (1/60) state p
 
-    print ""
+    return state { particles = newPs}
+handleEvent (EventKey (MouseButton LeftButton) up _ _mousePos) state = unsafePerformIO $ do
+    let newPs = map2D (changeColor _mousePos (makeColor 0.0078 0.5882 1 1)) (particles state)
     return state { particles = newPs}
 
-handleEvent (EventKey (MouseButton LeftButton) up _ _mousePos) state = unsafePerformIO $ do
-    let newPs = changeColor _mousePos black (particles state)
-    return state { particles = newPs}
+--End Debugging tools
+
+handleEvent (EventKey (Char 'p') Down _ _) state  = state { pause = not $ pause state }
+
 
 handleEvent (EventMotion _mousePos) state = state {mousePos = _mousePos}
 
 handleEvent (EventKey (SpecialKey KeySpace) Down _ _) state = state { generateParticles = True }
 handleEvent (EventKey (SpecialKey KeySpace) Up _ _) state = state { generateParticles = False }
 
+handleEvent (EventKey (Char 'G') Down _ _) state = state { showGrid = True }
+handleEvent (EventKey (Char 'G') Up _ _) state = state { showGrid = False }
 
+handleEvent (EventKey (Char 'g') Down _ _) state
+  | gravity state == 0 = state { gravity = -(981 * 5) }
+  | otherwise = state { gravity = 0 }
 
 handleEvent  _ state = state
 
 --End events
 
+
 applyPressure2 :: Float -> GameState -> Particle -> Point
 applyPressure2 dt state p = (density p, 0)
   where (vx,vy) = vel p
-        (vx',vy') 
+        (vx',vy')
           | density p < 0.00000001 = (0,0)
           -- | otherwise = customSum [(vx,vy) ,customMult pressureAcceleration dt]
           | otherwise = (sign ax * min 100 (abs (ax * dt)),sign ay * min 100 (abs (ay * dt)))
@@ -290,17 +327,16 @@ applyPressure2 dt state p = (density p, 0)
         -- pressureAcceleration = customMult (calculatePressureForce p state) (1 / density p)
         sign n = abs n / n
 
-changeColor :: Point -> Color -> [Particle] -> [Particle]
-changeColor _ c [p] = [p {particleColor = c}]
-changeColor (x1,y1) c (p:ps) 
-  | x1 >= x - defaultRadius && x1 <= x + defaultRadius && y1 >= y - defaultRadius && y1 <= y + defaultRadius = (p {particleColor = c}):ps
-  | otherwise = p:changeColor (x1,y1) c ps
+changeColor :: Point -> Color -> Particle ->  Particle
+changeColor (x1,y1) c p
+  | x1 >= x - defaultRadius && x1 <= x + defaultRadius && y1 >= y - defaultRadius && y1 <= y + defaultRadius = p {particleColor = c}
+  | otherwise = p
   where (x,y) = pos p
 
 
 closestParticle :: Point -> [Particle] -> Particle
 closestParticle _ [p] = p
-closestParticle (x1,y1) (p:ps) 
+closestParticle (x1,y1) (p:ps)
   | x1 >= x - smoothRadius  && x1 <= x + smoothRadius  && y1 >= y - smoothRadius  && y1 <= y + smoothRadius  = p
   | otherwise = closestParticle (x1,y1) ps
   where (x,y) = pos p
